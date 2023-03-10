9b280d600bffa7522649c7018ee910de9f21518c
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-03-10 15:22:24 +0800

    create passwordManager

---
+++ src/user/PasswordManager.java
@@ -0,0 +1,16 @@
+package user;
+
+import java.util.Objects;
+
+public class PasswordManager {
+    private String password;
+    PasswordManager(){
+        password = "password";
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public boolean checkPassword(String input){
+        return Objects.equals(input, password);
+    }
+}
--- src/user/User.java
+++ src/user/User.java
@@ -4,43 +4,6 @@ package user;
  * This class represents a user, which is the base class for other types of users.
  * It includes a user ID and a password field.
  */
-public class User {
-
-    /**
-     * The ID of the user.
-     */
-    private String userID;
-
-    /**
-     * The password for the user.
-     */
-    private String password;
-
-    /**
-     * Constructs a new User object with the specified user ID.
-     *
-     * @param userID the ID of the user.
-     */
-    public User(String userID) {
-        this.userID = userID;
-        password = "password";
-    }
-
-    /**
-     * Gets the user ID of the user.
-     *
-     * @return the ID of the user.
-     */
-    public String getUserID() {
-        return userID;
-    }
-
-    /**
-     * Sets the password for the user.
-     *
-     * @param password the new password for the user.
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
+public interface User {
+    public void setPassword();
 }
