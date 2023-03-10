80859ece30141088432aa6109bdc1b1b71b46f42
Pu Fanyi <pufanyi@gmail.com>
2023-03-10 15:51:55 +0800

    add Supervisor

--- src/user/Supervisor.java
+++ src/user/Supervisor.java
@@ -1,4 +1,51 @@
 package user;
 
-public class Supervisor implements User{
+/**
+ * This class represents a supervisor, which is a type of user.
+ * It extends the User class and includes a supervisor ID field.
+ */
+public class Supervisor implements User {
+    private final String supervisorID;
+    private final PasswordManager passwordManager;
+
+    /**
+     * Constructs a new Supervisor object with the specified supervisor ID.
+     *
+     * @param supervisorID the ID of the supervisor.
+     */
+    Supervisor(String supervisorID) {
+        this.supervisorID = supervisorID;
+        passwordManager = new PasswordManager();
+    }
+
+    /**
+     * Gets the user ID of the user.
+     *
+     * @return the ID of the user.
+     */
+    @Override
+    public String getUserID() {
+        return this.supervisorID;
+    }
+
+    /**
+     * Sets the password for the user.
+     *
+     * @param password the new password for the user.
+     */
+    @Override
+    public void setPassword(String password) {
+        passwordManager.setPassword(password);
+    }
+
+    /**
+     * Checks if the password is correct.
+     *
+     * @param password the password to check
+     * @return true if the password is correct, false otherwise
+     */
+    @Override
+    public boolean checkPassword(String password) {
+        return passwordManager.checkPassword(password);
+    }
 }
