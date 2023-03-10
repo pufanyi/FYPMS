b1376cf96f378ca91333d80981b952c558aaadc8
pufanyi <pufanyi@gmail.com>
2023-03-10 14:50:56 +0800

    fix the code style

--- src/user/Student.java
+++ src/user/Student.java
@@ -1,15 +1,17 @@
 package user;
 
-public class Student extends User{
+public class Student extends User {
     /**
      * studentID of the student
      */
     private String studentID;
+
     /**
      * constructor of a Student
+     *
      * @param studentID of a student
      */
-    public Student(String studentID){
+    public Student(String studentID) {
         super(studentID);
     }
 }
--- src/user/User.java
+++ src/user/User.java
@@ -9,23 +9,27 @@ public class User {
      * password of a user
      */
     private String password;
+
     /**
      * constructor of a user
+     *
      * @param userID of the user
      */
-    public User(String userID){
+    public User(String userID) {
         this.userID = userID;
         password = "password";
     }
+
     /**
      * get the userID of the user
+     *
      * @return userID of the user
      */
-    public String getUserID(){
+    public String getUserID() {
         return userID;
     }
+
     /**
-     *
      * @param password - set the password for the user
      */
     public void setPassword(String password) {
