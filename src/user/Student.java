de9abc78bb72d8b91bf4ec1ec0356b16ac7d656d
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-03-10 14:17:06 +0800

    create User and Student Class

---
+++ src/user/Student.java
@@ -0,0 +1,15 @@
+package user;
+
+public class Student extends User{
+    /**
+     * studentID of the student
+     */
+    private String studentID;
+    /**
+     * constructor of a Student
+     * @param studentID of a student
+     */
+    public Student(String studentID){
+        super(studentID);
+    }
+}
---
+++ src/user/User.java
@@ -0,0 +1,34 @@
+package user;
+
+public class User {
+    /**
+     * userID of a user
+     */
+    String userID;
+    /**
+     * password of a user
+     */
+    private String password;
+    /**
+     * constructor of a user
+     * @param userID of the user
+     */
+    public User(String userID){
+        this.userID = userID;
+        password = "password";
+    }
+    /**
+     * get the userID of the user
+     * @return userID of the user
+     */
+    public String getUserID(){
+        return userID;
+    }
+    /**
+     *
+     * @param password - set the password for the user
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
