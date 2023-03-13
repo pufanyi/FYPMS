c41be90eb795808d87fbb9c648eabe0ecb16cfd3
Pu Fanyi <pufanyi@gmail.com>
2023-03-14 00:55:17 +0800

    Update ProjectListTest.java

--- src/test/project/ProjectListTest.java
+++ src/test/project/ProjectListTest.java
@@ -1,23 +1,34 @@
 package test.project;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import project.ProjectList;
+import org.junit.jupiter.api.TestInstance;
 import project.Project;
+import project.ProjectList;
 import user.singleuser.Supervisor;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProjectListTest {
+    Project[] projects;
+
+    @BeforeAll
+    public void addAllProjects() {
+        Project project1 = new Project(233, "Project 1", new Supervisor("a", "b", "c"));
+        Project project2 = new Project(234, "Project 2", new Supervisor("ea", "f", "g"));
+        Project project3 = new Project(235, "Project 3", new Supervisor("h", "i", "j"));
+        projects = new Project[]{project1, project2, project3};
+    }
+
     @Test
     @DisplayName("Test Add Project")
     public void testAddProject() {
         ProjectList projectList = new ProjectList();
-        Project project1 = new Project(233, "Project 1", new Supervisor("a", "b", "c"));
-        Project project2 = new Project(234, "Project 2", new Supervisor("ea", "f", "g"));
-        Project project3 = new Project(235, "Project 3", new Supervisor("h", "i", "j"));
-        projectList.addProject(project1);
-        projectList.addProject(project2);
-        projectList.addProject(project3);
-        assertEquals(projectList.getProject(233), project1);
+        for (Project project : projects) {
+            projectList.addProject(project);
+        }
+        assertEquals(projects[0], projectList.getProject(projects[0].getProjectID()));
     }
 }
