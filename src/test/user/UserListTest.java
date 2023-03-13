6c0c8e306591714d9943221688495db4471b2f3e
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-03-13 16:16:51 +0800

    Update UserListTest.java

--- src/test/user/UserListTest.java
+++ src/test/user/UserListTest.java
@@ -1,8 +1,7 @@
 package test.user;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import user.singleuser.User;
+import user.singleuser.Student;
 import user.userlist.UserList;
 
 import org.junit.jupiter.api.Test;
@@ -10,111 +9,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class UserListTest {
-    private UserList<User> userList;
 
-    @BeforeEach
-    void setUp() {
-        userList = new UserList<>();
+    @Test
+    @DisplayName("Test add user")
+    public void addUserTest() {
+        UserList<Student> userList = new UserList<Student>();
+        Student student1 = new Student("A1234567A", "Lucas", "Lucas@e.ntu.edu.sg");
+        Student student2 = new Student("12345", "pufanyi","pufanyi@e.ntu.edu.sg");
+        Student student3 = new Student("78687", "jinqingyang", "jinqingyang@e.ntu.edu.sg");
+        userList.addUser(student1);
+        userList.addUser(student2);
+        userList.addUser(student3);
+        assertSame(student1, userList.getUser("A1234567A"));
+        assertNotSame(student1, userList.getUser("12345"));
     }
 
     @Test
-    void addUser() {
-        User user1 = createUser("1", "User 1", "user1@example.com", "password1");
-        User user2 = createUser("2", "User 2", "user2@example.com", "password2");
-
-        userList.addUser(user1);
-        userList.addUser(user2);
-
-        assertEquals(2, userList.getUser().size());
-        assertTrue(userList.getUser().contains(user1));
-        assertTrue(userList.getUser().contains(user2));
-    }
-
-    @Test
-    void removeUser() {
-        User user1 = createUser("1", "User 1", "user1@example.com", "password1");
-        User user2 = createUser("2", "User 2", "user2@example.com", "password2");
-
-        userList.addUser(user1);
-        userList.addUser(user2);
-
-        userList.removeUser(user1);
-
-        assertEquals(1, userList.getUser().size());
-        assertFalse(userList.getUser().contains(user1));
-        assertTrue(userList.getUser().contains(user2));
-    }
-
-    @Test
-    void getUser() {
-        User user1 = createUser("1", "User 1", "user1@example.com", "password1");
-        User user2 = createUser("2", "User 2", "user2@example.com", "password2");
-
-        userList.addUser(user1);
-        userList.addUser(user2);
-
-        User result = userList.getUser("2");
-
-        assertEquals(user2, result);
-    }
-
-    @Test
-    void removeUserWithUserID() {
-        User user1 = createUser("1", "User 1", "user1@example.com", "password1");
-        User user2 = createUser("2", "User 2", "user2@example.com", "password2");
-
-        userList.addUser(user1);
-        userList.addUser(user2);
-
-        boolean result = userList.removeUser("1");
-
-        assertTrue(result);
-        assertEquals(1, userList.getUser().size());
-        assertFalse(userList.getUser().contains(user1));
-        assertTrue(userList.getUser().contains(user2));
-    }
-
-    @Test
-    void removeUserWithUserIDReturnsFalseWhenUserNotFound() {
-        User user1 = createUser("1", "User 1", "user1@example.com", "password1");
-        User user2 = createUser("2", "User 2", "user2@example.com", "password2");
-
-        userList.addUser(user1);
-
-        boolean result = userList.removeUser("2");
-
-        assertFalse(result);
-        assertEquals(1, userList.getUser().size());
-        assertTrue(userList.getUser().contains(user1));
-        assertFalse(userList.getUser().contains(user2));
-    }
-
-    private User createUser(String id, String name, String email, String password) {
-        return new User() {
-            @Override
-            public String getUserID() {
-                return id;
-            }
-
-            @Override
-            public String getUserName() {
-                return name;
-            }
-
-            @Override
-            public void setPassword(String password) {
-                // do nothing
-            }
-
-            @Override
-            public boolean checkPassword(String password) {
-                return password.equals(password);
-            }
-
-            @Override
-            public String getEmail() {
-                return email;
-            }
-        };
+    @DisplayName("Test remove user")
+    public void removeUser(){
+        UserList<Student> userList = new UserList<Student>();
+        Student student1 = new Student("A1234567A", "Lucas", "Lucas@e.ntu.edu.sg");
+        Student student2 = new Student("12345", "pufanyi","pufanyi@e.ntu.edu.sg");
+        Student student3 = new Student("78687", "jinqingyang", "jinqingyang@e.ntu.edu.sg");
+        userList.addUser(student1);
+        userList.addUser(student2);
+        userList.addUser(student3);
+        
     }
 }
