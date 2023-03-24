467cf6dfbeec587535af844ec13cbed4f7c82462
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-03-24 16:22:30 +0800

    Request Status

--- src/main/model/request/Request.java
+++ src/main/model/request/Request.java
@@ -3,5 +3,9 @@ package main.model.request;
 import main.model.Model;
 
 public abstract class Request extends Model {
+    /**
+     * The status of the request.
+     */
+    private RequestStatus status;
     // TODO: Implement this class
 }
---
+++ src/main/model/request/RequestStatus.java
@@ -0,0 +1,7 @@
+package main.model.request;
+
+public enum RequestStatus {
+    APPROVED,
+    DENIED,
+    PENDING
+}
