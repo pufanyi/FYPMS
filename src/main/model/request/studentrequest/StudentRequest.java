40017dd2bfb8d2dfaf82c8a0946720a688975437
Pu Fanyi <pufanyi@gmail.com>
2023-03-24 17:05:34 +0800

    Add some todo list

---
+++ src/main/model/request/studentrequest/StudentRequest.java
@@ -0,0 +1,7 @@
+package main.model.request.studentrequest;
+
+import main.model.request.Request;
+
+public abstract class StudentRequest extends Request {
+    // TODO: implement this class
+}
---
+++ src/main/model/request/studentrequest/ViewProjectListRequest.java
@@ -0,0 +1,7 @@
+package main.model.request.studentrequest;
+
+import main.model.request.Request;
+
+public class ViewProjectListRequest extends StudentRequest {
+    // TODO: implement this class
+}
