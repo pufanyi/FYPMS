35514222cc4b363e8ebd1a0f82eaec4796ae8fde
Pu Fanyi <pufanyi@gmail.com>
2023-03-24 16:37:07 +0800

    update Request

--- src/main/model/request/Request.java
+++ src/main/model/request/Request.java
@@ -1,11 +1,25 @@
 package main.model.request;
 
 import main.model.Model;
+import main.repository.request.RequestRepository;
 
 public abstract class Request extends Model {
     /**
      * The status of the request.
      */
     private RequestStatus status;
-    // TODO: Implement this class
+
+    private String requestID;
+
+    Request() {
+        requestID = generateID();
+    }
+
+    public String generateID() {
+        return RequestRepository.getInstance().size() + "";
+    }
+
+    public String getID() {
+        return requestID;
+    }
 }
--- src/main/repository/request/RequestRepository.java
+++ src/main/repository/request/RequestRepository.java
@@ -1,5 +1,36 @@
 package main.repository.request;
 
-public class RequestRepository {
-    // TODO: implement this class
+import main.model.request.Request;
+import main.repository.Repository;
+import main.utils.config.Location;
+
+import java.util.List;
+import java.util.Map;
+
+public class RequestRepository extends Repository<Request> {
+    public static RequestRepository getInstance() {
+        return new RequestRepository();
+    }
+
+    RequestRepository() {
+        super();
+        load();
+    }
+
+    private static final String FILE_PATH = "/data/request/request.txt";
+
+    @Override
+    public String getFilePath() {
+        return Location.LOCATION + FILE_PATH;
+    }
+
+    /**
+     * Sets the list of mappable objects.
+     *
+     * @param listOfMappableObjects the list of mappable objects
+     */
+    @Override
+    public void setAll(List<Map<String, String>> listOfMappableObjects) {
+        // TODO: Implement this method
+    }
 }
