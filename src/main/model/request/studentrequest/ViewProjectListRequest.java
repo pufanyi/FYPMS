33ef7fa970f48befe7e194064bde86667ea0a508
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-03-25 11:46:48 +0800

    add request interfaces

--- src/main/model/request/studentrequest/ViewProjectListRequest.java
+++ src/main/model/request/studentrequest/ViewProjectListRequest.java
@@ -1,7 +1,8 @@
 package main.model.request.studentrequest;
 
 import main.model.request.Request;
+import main.model.request.RequestView;
 
-public class ViewProjectListRequest extends StudentRequest {
+public class ViewProjectListRequest extends StudentRequest implements RequestView {
     // TODO: implement this class
 }
