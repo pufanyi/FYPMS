cbbf256476cf8410018338927037b32d49ae1105
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-03-25 13:30:46 +0800

    Update ChangeSupervisorRequest.java

--- src/main/model/request/coordinatorrequest/ChangeSupervisorRequest.java
+++ src/main/model/request/coordinatorrequest/ChangeSupervisorRequest.java
@@ -5,13 +5,30 @@ import main.model.request.RequestChange;
 import java.util.Map;
 
 public class ChangeSupervisorRequest extends CoordinatorRequest implements RequestChange {
-    @Override
-    public Map<String, String> toMap() {
-        return null;
+    private String supervisorID;
+
+    public ChangeSupervisorRequest(String supervisorID) {
+        super();
+        this.supervisorID = supervisorID;
+    }
+
+    public ChangeSupervisorRequest(Map<String, Object> map) {
+        super(map);
+        this.supervisorID = (String) map.get("supervisorID");
+    }
+
+    public String getSupervisorID() {
+        return supervisorID;
+    }
+
+    public void setSupervisorID(String supervisorID) {
+        this.supervisorID = supervisorID;
     }
 
     @Override
-    public void fromMap(Map<String, String> map) {
-
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = super.toMap();
+        map.put("supervisorID", supervisorID);
+        return map;
     }
 }
