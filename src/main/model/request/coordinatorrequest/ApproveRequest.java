67fbef79ddd6796cb09de1663f3f580a207be8b6
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-03-25 21:33:58 +0800

    add

---
+++ src/main/model/request/coordinatorrequest/ApproveRequest.java
@@ -0,0 +1,64 @@
+package main.model.request.coordinatorrequest;
+
+import main.model.request.ChangeRequest;
+import main.model.request.Request;
+import main.model.request.RequestStatus;
+import main.repository.request.RequestRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApproveRequest extends CoordinatorRequest implements ChangeRequest {
+    /**
+     * The requestID to be approved
+     */
+    private String requestID;
+    /**
+     * The type of the request
+     */
+    private static final String requestType = "Approve request";
+    /**
+     * The status of the request
+     */
+    private RequestStatus status;
+    /**
+     * The request to be approved
+     */
+    private Request request;
+
+    /**
+     * Constructs a new ApproveRequest object with the specified request.
+     * @param requestID the requestID to be approved
+     */
+    public ApproveRequest(String requestID) {
+        super();
+        this.requestID = requestID;
+    }
+
+    @Override
+    public Map<String, String> toMap() {
+        Map<String, String> ans = new HashMap<>();
+        ans.put("requestType", requestType);
+        ans.put("requestID", requestID);
+        ans.put("status", request.getStatus().toString());
+        return ans;
+    }
+
+    @Override
+    public void fromMap(Map<String, String> map) {
+
+    }
+
+    /**
+     * Approve the request
+     * @throws IllegalStateException if the request is not pending
+     */
+    public void approve() throws IllegalStateException{
+        request = RequestRepository.getInstance().getByID(requestID);
+        if(request.getStatus() != RequestStatus.PENDING)
+            throw new IllegalStateException("Request is not pending");
+        else if(request.getStatus() == RequestStatus.APPROVED)
+            throw new IllegalStateException("Request is already approved");
+        request.setStatus(RequestStatus.APPROVED);
+    }
+}
