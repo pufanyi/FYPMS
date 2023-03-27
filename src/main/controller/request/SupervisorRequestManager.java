129f690825e02eb143dc0c257d64862585b36f45
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-03-28 00:24:13 +0800

    edit request

--- src/main/controller/request/SupervisorRequestManager.java
+++ src/main/controller/request/SupervisorRequestManager.java
@@ -1,4 +1,18 @@
 package main.controller.request;
 
+import main.model.request.Request;
+import main.model.request.supervirsorrequest.TransferStudentRequest;
+import main.repository.request.RequestRepository;
+import main.utils.exception.repository.ModelAlreadyExistsException;
+
 public class SupervisorRequestManager {
+    public void transferToNewSupervisor(String projectID, String supervisorID, String newSupervisorID, String studentID) {
+        String requestID = RequestRepository.getInstance().size() + "";
+        Request request = new TransferStudentRequest(requestID, projectID, supervisorID, newSupervisorID, studentID);
+        try {
+            RequestRepository.getInstance().add(request);
+        } catch (ModelAlreadyExistsException e) {
+            e.printStackTrace();
+        }
+    }
 }
