cfe7b0a65c12279d55b4c1845ea0a0d45b0c7ce1
Pu Fanyi <pufanyi@gmail.com>
2023-03-28 23:39:29 +0800

    Update ViewAvailableProjectListTest.java

--- src/test/ui/student/details/ViewAvailableProjectListTest.java
+++ src/test/ui/student/details/ViewAvailableProjectListTest.java
@@ -2,10 +2,12 @@ package test.ui.student.details;
 
 import main.boundary.student.details.ViewAvailableProjectList;
 import main.controller.project.ProjectManager;
+import main.repository.project.ProjectRepository;
 import main.utils.exception.repository.ModelAlreadyExistsException;
 
 public class ViewAvailableProjectListTest {
     public static void main(String[] args) throws ModelAlreadyExistsException {
+        ProjectRepository.getInstance().clear();
         ProjectManager.createProject("1", "Haha", "Haha");
         ViewAvailableProjectList.viewAvailableProjectList();
     }
