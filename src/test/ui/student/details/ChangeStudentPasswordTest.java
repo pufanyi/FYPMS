a60acf16a947e07011e283f197a5705423a3fce1
Pu Fanyi <pufanyi@gmail.com>
2023-03-28 21:01:40 +0800

    Update ChangeStudentPasswordTest.java

--- src/test/ui/student/details/ChangeStudentPasswordTest.java
+++ src/test/ui/student/details/ChangeStudentPasswordTest.java
@@ -3,15 +3,17 @@ package test.ui.student.details;
 import main.boundary.account.LoginUI;
 import main.boundary.student.details.ChangeStudentPassword;
 import main.controller.account.AccountManager;
+import main.model.user.Student;
 import main.model.user.UserType;
 import main.repository.user.CoordinatorRepository;
+import main.repository.user.StudentRepository;
 import main.utils.exception.repository.ModelAlreadyExistsException;
 import main.utils.exception.ui.PageBackException;
 
 public class ChangeStudentPasswordTest {
-    public static void main(String args[]) throws ModelAlreadyExistsException, PageBackException {
-        CoordinatorRepository.getInstance().clear();
-        AccountManager.register(UserType.COORDINATOR, "D220006", "Nicole", "D220006@e.ntu.edu.sg");
+    public static void main(String[] args) throws ModelAlreadyExistsException, PageBackException {
+        StudentRepository.getInstance().clear();
+        AccountManager.register(UserType.STUDENT, "D220006", "Nicole", "D220006@e.ntu.edu.sg");
         try {
             ChangeStudentPassword.changeStudentPassword("D220006");
         } catch (PageBackException e) {
