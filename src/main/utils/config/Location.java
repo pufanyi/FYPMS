b1130a14f9e20c9f526531e9cca707067e628fef
Pu Fanyi <pufanyi@gmail.com>
2023-03-31 17:49:35 +0800

    update the docs of the Location class

--- src/main/utils/config/Location.java
+++ src/main/utils/config/Location.java
@@ -6,6 +6,7 @@ package main.utils.config;
 public class Location {
     /**
      * The location of the resources.
+     * If the resources are in the same directory as the program, this should be ".".
      */
     public static final String RESOURCE_LOCATION = "."; // ""D:\\NTU\\Y1S2\\SC2002\\FYPMS";
 }
