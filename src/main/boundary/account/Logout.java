d9c2a337e5a9213e4953fc618244031a379161ee
Pu Fanyi <pufanyi@gmail.com>
2023-04-05 14:48:19 +0800

    logout

---
+++ src/main/boundary/account/Logout.java
@@ -0,0 +1,11 @@
+package main.boundary.account;
+
+import main.boundary.welcome.Welcome;
+
+import static java.lang.System.exit;
+
+public class Logout {
+    public static void logout() {
+        Welcome.welcome();
+    }
+}
--- src/main/boundary/welcome/ExitPage.java
+++ src/main/boundary/welcome/ExitPage.java
@@ -1,7 +1,10 @@
 package main.boundary.welcome;
 
+import main.utils.ui.ChangePage;
+
 public class ExitPage {
     public static void exitPage() {
+        ChangePage.changePage();
         System.out.println("Thank you for using our system!");
         System.exit(0);
     }
