c1897b744412909149244f28619977789667ed9e
jin-qingyang <124124548+jin-qingyang@users.noreply.github.com>
2023-04-15 21:00:43 +0800

    more注释

--- src/test/boundary/LoginUITest.java
+++ src/test/boundary/LoginUITest.java
@@ -7,7 +7,22 @@ import main.repository.user.StudentRepository;
 import main.utils.exception.ModelAlreadyExistsException;
 import main.utils.exception.PageBackException;
 
+
+/**
+
+ A test class that tests the functionality of the LoginUI class. It clears the student repository, registers a student,
+ then attempts to login with the registered credentials.
+ */
 public class LoginUITest {
+    /**
+
+     The main function to test the login functionality of the LoginUI class.
+     It clears the student repository, registers a new student with a userID of "FPU001",
+     a name of "Pu Fanyi", and an email of "FPU001@e.ntu.edu.sg". Then it calls the LoginUI
+     login function to test if the login functionality works correctly.
+     @throws PageBackException if the user navigates back to the previous page
+     @throws ModelAlreadyExistsException if the model to be registered already exists in the repository
+     */
     public static void main(String[] args) throws PageBackException, ModelAlreadyExistsException {
         StudentRepository.getInstance().clear();
         AccountManager.register(
--- src/test/boundary/PasswordTest.java
+++ src/test/boundary/PasswordTest.java
@@ -2,6 +2,11 @@ package test.boundary;
 
 import main.boundary.account.AttributeGetter;
 
+/**
+
+ A test class that retrieves the password from user input using {@link main.boundary.account.AttributeGetter}
+ and prints it to the console.
+ */
 public class PasswordTest {
     public static void main(String[] args) {
         String password = AttributeGetter.getPassword();
